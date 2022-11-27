facf8f95cdb51a3120779497691a155d77a3f83d
Sandesh Lamsal <sandeshlamsal714@gmail.com>
2022-11-26 22:52:30 -0600

    Initial Commit with starter code and folders

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import NavgBar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="NavgBar">
+        <NavgBar/>
+      </div>
+      <div className="main">
+        <h1> Hello World</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
